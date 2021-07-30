45cc5b3973fb0a7e6affe7a0f46415bbe6aa63bf
irtizak <irtizakaleem@gmail.com>
2021-07-30 19:35:20 -0400

    Updated Streamlit App

--- app.py
+++ app.py
@@ -1,56 +1,51 @@
+# Import libraries
 import joblib
 import streamlit as st
 import numpy as np
 import pandas as pd
 
-reg = joblib.load('model/model.pkl')
+# Import models
+rgr = joblib.load('model/rgr.pkl')
+clf = joblib.load('model/clf.pkl')
 
-def predict(model, input_arr):
-    prediction = model.predict(input_arr)[0]
+# Prediction function
+def predict(model, df):
+    prediction = model.predict(df)[0]
     return prediction
 
 def run():
-    # from PIL import Image
-    # image = Image.open('logo.png')
-    # image_flower = Image.open('flower.png')
 
-    # st.Image(image, use_column_width=False)
-
-    add_selectbox = st.sidebar.selectbox(
-        'How would you like to predict?',
-        ('Online', 'Batch')
-    )
-
-    st.sidebar.info('This app is created to predict salaries')
+    st.sidebar.info('This app predicts faculty salaries.')
     st.sidebar.success('http://www.github.com/irtizak/salaries')
-
-    # st.sidebar.image('flower.png')
+    
+    add_selectbox = st.sidebar.selectbox(
+        'What model would you like to use?',
+        ('Regression', 'Classification')
+    )
 
     st.title('Salary Prediction App')
 
-    if add_selectbox == 'Online':
-        yrs_since_phd = st.number_input('Years Since Ph.D. and Masters and Whatever', min_value=0., max_value=60., value=22.)
-        yrs_service = st.number_input('Years of Service in the Navy', min_value=0., max_value=60., value=17.)
+    yrs_since_phd = st.number_input('Years Since Ph.D.', min_value=0., max_value=60., value=22.)
+    yrs_service = st.number_input('Years of Service', min_value=0., max_value=60., value=17.)
+    rank = st.selectbox('Rank', ('AsstProf', 'AssocProf', 'Prof'))
+    discipline = st.selectbox('Discipline', ('A', 'B'))
+    sex = st.selectbox('Sex', ('Male', 'Female'))
 
-        output=''
-
-        input_arr = np.array([yrs_since_phd, yrs_service]).reshape(1, -1)
-        
-        output_dict = {0: 'setosa', 1: 'versicolor', 2: 'virginica'}
-
-        if st.button('Predict'):
-            output = predict(reg, input_arr)
-#             output = output_dict[output]
-        
-        st.success('The predicted salary is {}'.format(output))
-
-    # if add_selection == 'Batch':
-    #     file_upload = st.file_uploader('Upload CSV file for predictions', type=['csv'])
-
-    #     if file_upload is not None:
-    #         data = pd.read_csv(file_upload)
-    #         predictions = predict(model, input_df)
-    #         st.write(predictions)
+    output=''
+    
+    col_names = ['yrs_since_phd', 'yrs_service', 'rank', 'discipline', 'sex']
+    input_data = [[yrs_since_phd], [yrs_service], [rank], [discipline], [sex]]
+    data_dict = dict(zip(col_names, input_data))
+    df = pd.DataFrame.from_dict(data_dict)
+    
+    if st.button('Predict'):
+        if add_selectbox == 'Regression':
+            output = round(predict(rgr, df), 0)
+            st.success('The predicted salary is {}'.format(output))
+        else:
+            output = round(predict(clf, df), 0)
+            output_dict = {1: 'ABOVE', 0: 'BELOW'}
+            st.success('{} median salary.'.format(output_dict[output]))
 
 if __name__ == '__main__':
     run()
